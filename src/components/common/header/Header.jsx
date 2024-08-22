3f79036bb03396024d5ffddc3c4a6260de576f33
Palak <98663148+palak-7@users.noreply.github.com>
2024-08-22 14:03:26 +0530

    made navbar responsive

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -17,22 +17,10 @@ const Header = () => {
     setNavbarOpen(!navbarOpen);
   };
 
-  // Sticky Navbar
-  // const [sticky, setSticky] = useState(false);
-  // const handleStickyNavbar = () => {
-  //   if (window.scrollY >= 80) {
-  //     setSticky(true);
-  //   } else {
-  //     setSticky(false);
-  //   }
-  // };
-  // useEffect(() => {
-  //   window.addEventListener("scroll", handleStickyNavbar);
-  //   return () => window.removeEventListener("scroll", handleStickyNavbar);
-  // }, []);
-
   // Submenu handler
   const [openIndex, setOpenIndex] = useState(-1);
+  const [openSubIndex, setSubOpenIndex] = useState(-1);
+
   const handleSubmenu = (index) => {
     if (openIndex === index) {
       setOpenIndex(-1);
@@ -40,15 +28,22 @@ const Header = () => {
       setOpenIndex(index);
     }
   };
-
+  const handleSubSubmenu = (index) => {
+    console.log(index);
+    if (openSubIndex === index) {
+      setSubOpenIndex(-1);
+    } else {
+      setSubOpenIndex(index);
+    }
+  };
   const usePathName = usePathname();
 
   return (
     <div
-      className={`overflow-x-clip header left-0 z-40 top-0 w-full items-center bg-gradient-to-r font-semibold bg-white `}
+      className={`overflow-x-clip header left-0 z-40 top-0 w-full items-center bg-gradient-to-r font-semibold`}
     >
       <Top />
-      <div className="grid grid-cols-2">
+      <div className="lg:grid grid-cols-2">
         <div className="flex justify-center">
           <Link href="/" className={`header-logo block lg:ml-0 ml-2 my-3`}>
             <Image
@@ -128,66 +123,69 @@ const Header = () => {
       <header className="flex justify-around bg-lightblue text-white">
         <div className="container flex items-center">
           <div className="flex items-center w-full justify-between lg:w-auto mx-5"></div>
-          <div className="flex items-center w-full justify-center lg:flex-grow">
+          <div className=" flex items-center w-full justify-center lg:flex-grow">
             <button
               onClick={navbarToggleHandler}
               id="navbarToggler"
               aria-label="Mobile Menu"
-              className="rounded-full px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
+              className=" rounded-full px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
             >
               <span
-                className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
+                className={`relative my-1.5 block h-0.5 w-[30px] bg-white transition-all duration-300  ${
                   navbarOpen ? " top-[7px] rotate-45" : ""
                 }`}
               />
               <span
-                className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
+                className={`relative my-1.5 block h-0.5 w-[30px] bg-white transition-all duration-300 ${
                   navbarOpen ? "opacity-0" : ""
                 }`}
               />
               <span
-                className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
+                className={`relative my-1.5 block h-0.5 w-[30px] bg-white transition-all duration-300  ${
                   navbarOpen ? " top-[-8px] -rotate-45" : ""
                 }`}
               />
             </button>
             <nav
               id="navbarCollapse"
-              className={`navbar absolute right-0 z-30 rounded border-[.5px] border-body-color/50 bg-white px-6 py-4 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static w-full lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${
+              className={`navbar absolute right-0 z-30 rounded border-[.5px] border-body-color/50 px-6 py-4 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static w-full lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${
                 navbarOpen
-                  ? "visibility top-full opacity-100"
+                  ? "visibility top-[45%] opacity-100"
                   : "invisible top-[120%] opacity-0"
               }`}
             >
-              <ul className="block lg:grid grid-cols-6 gap-4 relative">
+              <ul className="block lg:grid grid-cols-6 gap-4 relative bg-lightblue">
                 {menuData.map((menuItem, index) => (
                   <li
+                    onMouseEnter={() => handleSubmenu(menuItem.id)}
+                    onMouseLeave={() => {
+                      handleSubmenu(-1);
+                      handleSubSubmenu(-1);
+                    }}
                     key={index}
-                    className="flex items-center justify-center group relative"
+                    className={`group relative text-center ${
+                      usePathName === menuItem.path
+                        ? "bg-lightgreen"
+                        : "text-dark hover:bg-lightgreen"
+                    }`}
                   >
                     {menuItem.path ? (
                       <Link
                         href={menuItem.path}
-                        className={`w-full flex py-2 text-base lg:mr-0 lg:inline-flex lg:px-0 lg:py-2 ${
-                          usePathName === menuItem.path
-                            ? "bg-primary h-full lg:px-2 dark:text-white"
-                            : "text-dark hover:bg-primary hover:h-full dark:text-white/70 dark:hover:text-white"
-                        }`}
+                        className={`flex py-2 px-2 text-base lg:mr-0 lg:inline-flex lg:px-0 lg:py-3 `}
                       >
-                        <span className="flex-grow text-center">
-                          {menuItem.title}
-                        </span>
+                        <div>{menuItem.title}</div>
                       </Link>
                     ) : (
                       <>
                         <p
-                          onClick={() => handleSubmenu(index)}
-                          className="flex items-center justify-center cursor-pointer py-2 text-base text-dark group-hover:bg-primary group-hover:px-2 dark:text-white/70 dark:group-hover:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-3"
+                          onClick={() => {
+                            handleSubmenu(menuItem.id);
+                          }}
+                          className=" px-2 flex cursor-pointer items-center justify-between py-2 text-base text-dark group-hover:bg-lightgreen lg:mr-0 lg:inline-flex lg:px-0 lg:py-3"
                         >
-                          <span className="flex-grow text-center">
-                            {menuItem.title}
-                          </span>
-                          <span className="pl-3">
+                          {menuItem.title}
+                          <span className="pl-3 ">
                             <svg width="25" height="24" viewBox="0 0 25 24">
                               <path
                                 fillRule="evenodd"
@@ -199,20 +197,72 @@ const Header = () => {
                           </span>
                         </p>
                         <div
-                          className={`submenu absolute left-0 top-full mt-1 rounded-sm bg-lightblue transition-all duration-300 shadow-lg group-hover:opacity-100 dark:bg-dark lg:w-[250px] lg:p-4 lg:opacity-0 ${
-                            openIndex === index
-                              ? "opacity-100 visible"
-                              : "opacity-0 invisible"
+                          className={`submenu relative left-0 top-full lg:ml-0 pl-4 rounded-sm transition-[top] duration-300 lg:absolute lg:top-[102%] lg:w-[250px] lg:p-4 bg-lightblue lg:shadow-lg ${
+                            openIndex == menuItem.id ? "block" : "hidden"
                           }`}
                         >
                           {menuItem.submenu.map((submenuItem, index) => (
-                            <Link
-                              href={submenuItem.path}
+                            <div
                               key={index}
-                              className="block rounded py-2.5 text-sm text-dark hover:scale-105 dark:text-white/70 dark:hover:text-white lg:px-3 text-center"
+                              className="group text-left relative bg-lightblue grid"
                             >
-                              {submenuItem.title}
-                            </Link>
+                              {submenuItem.path ? (
+                                <Link
+                                  href={submenuItem.path}
+                                  key={index}
+                                  className={`block rounded py-2 text-sm text-dark hover:bg-lightgreen lg:px-3`}
+                                >
+                                  {submenuItem.title}
+                                </Link>
+                              ) : (
+                                <>
+                                  <p
+                                    onMouseEnter={() => {
+                                      handleSubSubmenu(submenuItem.id);
+                                    }}
+                                    onClick={() => {
+                                      handleSubSubmenu(submenuItem.id);
+                                    }}
+                                    className="rounded py-2 text-sm lg:px-3 flex cursor-pointer justify-between text-dark hover:bg-lightgreen"
+                                  >
+                                    {submenuItem.title}
+                                    <span className="pl-3">
+                                      <svg
+                                        width="25"
+                                        height="24"
+                                        viewBox="0 0 25 24"
+                                      >
+                                        <path
+                                          fillRule="evenodd"
+                                          clipRule="evenodd"
+                                          d="M6.29289 8.8427C6.68342 8.45217 7.31658 8.45217 7.70711 8.8427L12 13.1356L16.2929 8.8427C16.6834 8.45217 17.3166 8.45217 17.7071 8.8427C18.0976 9.23322 18.0976 9.86639 17.7071 10.2569L12 15.964L6.29289 10.2569C5.90237 9.86639 5.90237 9.23322 6.29289 8.8427Z"
+                                          fill="currentColor"
+                                        />
+                                      </svg>
+                                    </span>
+                                  </p>
+                                  <div
+                                    className={`lg:ml-[225px] rounded-sm bg-lightblue lg:px-3 lg:py-2 lg:-mt-6 ml-4 transition-[top] duration-300 lg:absolute lg:w-[250px] lg:shadow-lg ${
+                                      openSubIndex === submenuItem.id
+                                        ? "block"
+                                        : "hidden"
+                                    }`}
+                                  >
+                                    {submenuItem.submenu.map(
+                                      (subsubmenuItem, index) => (
+                                        <Link
+                                          href={subsubmenuItem.path}
+                                          key={index}
+                                          className="bg-lightblue block rounded py-2.5 text-sm text-dark hover:bg-lightgreen lg:px-3"
+                                        >
+                                          {subsubmenuItem.title}
+                                        </Link>
+                                      )
+                                    )}
+                                  </div>
+                                </>
+                              )}
+                            </div>
                           ))}
                         </div>
                       </>
