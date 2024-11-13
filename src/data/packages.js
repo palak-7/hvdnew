30972699063fe17aecc849968bd7f0c602ba4007
Abhishekmathur00 <148318334+Abhishekmathur00@users.noreply.github.com>
2024-11-13 17:24:12 +0530

    json data correction

--- src/data/packages.js
+++ src/data/packages.js
@@ -1,372 +1,197 @@
 export const HealthPackage = [
   {
-    id: 1,
-    name: "HVD ESSENTIAL",
-    url: "hvd-essential",
-    image: "/packages/new-1.png",
-    number: 26,
-    description: [
+    "id": 1,
+    "name": "HVD ESSENTIAL",
+    "url": "hvd-essential",
+    "image": "/packages/new-1.png",
+    "number": 26,
+    "description": [
       {
-        heading: "Complete Blood Count",
-        content:
-          "Hemoglobin, TLC, DLC (Neutrophils, Lymphocytes, Basophils, Monocytes, Eosinophils), RBC count, PCV, MCV, MCH, MCHC, Platelet count, RDW",
+        "heading": "Complete Blood Count",
+        "content": "Hemoglobin, TLC, DLC (Neutrophils, Lymphocytes, Basophils, Monocytes, Eosinophils), RBC count, PCV, MCV, MCH, MCHC, Platelet count, RDW"
       },
       {
-        heading: "Diabetes Profile",
-        content: "HbA1c, Fasting Glucose, Estimated Average Glucose",
+        "heading": "Diabetes Profile",
+        "content": "HbA1c, Fasting Glucose, Estimated Average Glucose"
       },
       {
-        heading: "Lipid Profile",
-        content:
-          "Triglycerides, Total Cholesterol, HDL, LDL, VLDL, Total/HDL, LDL/HDL ratio",
+        "heading": "Lipid Profile",
+        "content": "Triglycerides, Total Cholesterol, HDL, LDL, VLDL, Total/HDL, LDL/HDL ratio"
       },
       {
-        heading: "Thyroid Profile",
-        content: "TSH, FT3, FT4",
+        "heading": "Thyroid Profile",
+        "content": "TSH, FT3, FT4"
       },
       {
-        heading: "Kidney Profile",
-        content: "Urea, Creatinine, Uric acid, BUN, NA, K, Calcium",
+        "heading": "Kidney Profile",
+        "content": "Urea, Creatinine, Uric acid, BUN, NA, K, Calcium"
       },
       {
-        heading: "Liver Profile",
-        content:
-          "Total Bilirubin, Direct Bilirubin, Indirect Bilirubin, SGOT, SGPT, Alkaline Phosphatase, GGT, Total Protein, Albumin, Globulin, A/G ratio",
+        "heading": "Liver Profile",
+        "content": "Total Bilirubin, Direct Bilirubin, Indirect Bilirubin, SGOT, SGPT, Alkaline Phosphatase, GGT, Total Protein, Albumin, Globulin, A/G ratio"
       },
       {
-        heading: "Bone Health",
-        content: "Calcium, Phosphorus",
+        "heading": "Bone Health",
+        "content": "Calcium, Phosphorus"
       },
       {
-        heading: "Urine R/M",
-        content:
-          "Colour, Transparency, Specific gravity, pH, Albumin, Sugar, Pus cells, RBC, Casts, Crystals, Epithelial cells, Bacteria, Others",
-      },
+        "heading": "Urine R/M",
+        "content": "Colour, Transparency, Specific gravity, pH, Albumin, Sugar, Pus cells, RBC, Casts, Crystals, Epithelial cells, Bacteria, Others"
+      }
     ],
-    price: "1999",
-    cancelled: "2400",
-    newInfo: "Add Chest X RAY, ECG and Ultrasound Abdomen for just Rs. 2000"
+    "price": "1999",
+    "cancelled": "2400",
+    "newInfo": "Add Chest X RAY, ECG and Ultrasound Abdomen for just Rs. 2000"
   },
   {
-    id: 2,
-    name: "HVD ADVANCE",
-    url: "hvd-advance",
-    image: "/packages/new-2.png",
-    number: 56,
-    description: [
+    "id": 2,
+    "name": "HVD ADVANCE",
+    "url": "hvd-advance",
+    "image": "/packages/new-2.png",
+    "number": 56,
+    "description": [
       {
-        heading: "Complete Blood Count",
-        content:
-          "Hemoglobin, TLC, DLC (Neutrophils, Lymphocytes, Basophils, Monocytes, Eosinophils), RBC count, PCV, MCV, MCH, MCHC, Platelet count, RDW",
+        "heading": "Complete Blood Count",
+        "content": "Hemoglobin, TLC, DLC (Neutrophils, Lymphocytes, Basophils, Monocytes, Eosinophils), RBC count, PCV, MCV, MCH, MCHC, Platelet count, RDW"
       },
       {
-        heading: "Diabetes Profile",
-        content: "HbA1c, Fasting Glucose, Estimated Average Glucose",
+        "heading": "Diabetes Profile",
+        "content": "HbA1c, Fasting Glucose, Estimated Average Glucose"
       },
       {
-        heading: "Lipid Profile",
-        content:
-          "Triglycerides, Total Cholesterol, HDL, LDL, VLDL, Total/HDL, LDL/HDL ratio",
+        "heading": "Lipid Profile",
+        "content": "Triglycerides, Total Cholesterol, HDL, LDL, VLDL, Total/HDL, LDL/HDL ratio"
       },
       {
-        heading: "Thyroid Profile",
-        content: "TSH, FT3, FT4",
+        "heading": "Thyroid Profile",
+        "content": "TSH, FT3, FT4"
       },
       {
-        heading: "Kidney Profile",
-        content: "Urea, Creatinine, Uric acid, BUN, NA, K, Calcium",
+        "heading": "Kidney Profile",
+        "content": "Urea, Creatinine, Uric acid, BUN, NA, K, Calcium"
       },
       {
-        heading: "Liver Profile",
-        content:
-          "Total Bilirubin, Direct Bilirubin, Indirect Bilirubin, SGOT, SGPT, Alkaline Phosphatase, GGT, Total Protein, Albumin, Globulin, A/G ratio",
+        "heading": "Liver Profile",
+        "content": "Total Bilirubin, Direct Bilirubin, Indirect Bilirubin, SGOT, SGPT, Alkaline Phosphatase, GGT, Total Protein, Albumin, Globulin, A/G ratio"
       },
       {
-        heading: "Bone Health",
-        content: "Calcium, Phosphorus, Vitamin D",
+        "heading": "Bone Health",
+        "content": "Calcium, Phosphorus, Vitamin D"
       },
       {
-        heading: "Nervous System",
-        content: "Vitamin B12",
+        "heading": "Nervous System",
+        "content": "Vitamin B12"
       },
       {
-        heading: "IRON PROFILE",
-        content: "Calcium, Phosphorus, Vitamin D",
+        "heading": "IRON PROFILE",
+        "content": "Calcium, Phosphorus, Vitamin D"
       },
       {
-        heading: "Urine R/M",
-        content:
-          "Colour, Transparency, Specific gravity, pH, Albumin, Sugar, Pus cells, RBC, Casts, Crystals, Epithelial cells, Bacteria, Others",
-      },
+        "heading": "Urine R/M",
+        "content": "Colour, Transparency, Specific gravity, pH, Albumin, Sugar, Pus cells, RBC, Casts, Crystals, Epithelial cells, Bacteria, Others"
+      }
     ],
-    price: "2999 ",
-    cancelled: "4400",
-    newInfo: "Chest X RAY, ECG and Ultrasound Abdomen for just Rs. 2000"
+    "price": "2999 ",
+    "cancelled": "4400",
+    "newInfo": "Chest X RAY, ECG and Ultrasound Abdomen for just Rs. 2000"
   },
   {
-    id: 3,
-    name: "HVD CARDIAC HEALTH PACKAGE",
-    url: "hvd-cardiac",
-    image: "/packages/new-9.png",
-    description: [
-      {
-        heading: "ECG",
-        content: "",
-      },
-      {
-        heading: "ECHO",
-        content: "",
-      },
-      {
-        heading: "TMT",
-        content: "",
-      },
-      {
-        heading: "Sugar Random, HBA1C, Average Blood Sugar",
-        content: "",
-      },
-      {
-        heading: "HS CRP",
-        content: "",
-      },
-      {
-        heading: "Lipid Profile",
-        content: "",
-      },
+    "id": 3,
+    "name": "HVD CARDIAC HEALTH PACKAGE",
+    "url": "hvd-cardiac",
+    "image": "/packages/new-9.png",
+    "description": [
+      { "heading": "ECG", "content": "" },
+      { "heading": "ECHO", "content": "" },
+      { "heading": "TMT", "content": "" },
+      { "heading": "Sugar Random, HBA1C, Average Blood Sugar", "content": "" },
+      { "heading": "HS CRP", "content": "" },
+      { "heading": "Lipid Profile", "content": "" }
     ],
-    price: "4999",
-    cancelled: "12000",
+    "price": "4999",
+    "cancelled": "12000"
   },
   {
-    id: 4,
-    name: "HVD FEVER PACKAGE",
-    url: "hvd-fever",
-    image: "/packages/pack8.png",
-    description: [
+    "id": 4,
+    "name": "HVD FEVER PACKAGE",
+    "url": "hvd-fever",
+    "image": "/packages/pack8.png",
+    "description": [
       {
-        heading: "Complete Blood Count with ESR",
-        content:
-          "Hemoglobin, TLC, DLC (Neutrophils, Lymphocytes, Basophils, Monocytes, Eosinophils), RBC count, PCV, MCV, MCH, MCHC, Platelet count, RDW",
+        "heading": "Complete Blood Count with ESR",
+        "content": "Hemoglobin, TLC, DLC (Neutrophils, Lymphocytes, Basophils, Monocytes, Eosinophils), RBC count, PCV, MCV, MCH, MCHC, Platelet count, RDW"
       },
+      { "heading": "Widal Slide", "content": "" },
+      { "heading": "PS for Malaria Parasite", "content": "" },
+      { "heading": "Rapid Malaria Antigen", "content": "" },
+      { "heading": "Typhidot IgM/IgG", "content": "" },
+      { "heading": "NS – 1 Dengue Antigen", "content": "" },
       {
-        heading: "Widal Slide",
-        content: "",
-      },
-      {
-        heading: "PS for Malaria Parasite",
-        content: "",
-      },
-      {
-        heading: "Rapid Malaria Antigen",
-        content: "",
-      },
-      {
-        heading: "Typhidot IgM/IgG",
-        content: "",
-      },
-      {
-        heading: "NS – 1 Dengue Antigen",
-        content: "",
-      },
-      {
-        heading: "Urine R/M",
-        content:
-          "Colour, Transparency, Specific gravity, pH, Albumin, Sugar, Pus cells, RBC, Casts, Crystals, Epithelial cells, Bacteria, Others",
-      },
+        "heading": "Urine R/M",
+        "content": "Colour, Transparency, Specific gravity, pH, Albumin, Sugar, Pus cells, RBC, Casts, Crystals, Epithelial cells, Bacteria, Others"
+      }
     ],
-    price: "1999",
-    cancelled: "3000",
+    "price": "1999",
+    "cancelled": "3000"
   },
   {
-    id: 5,
-    name: "HVD ANC 1 PACKAGE",
-    url: "hvd-anc1-package",
-    image: "/packages/anc1.jpg",
-    description: [
-      {
-        heading: "Complete Blood Count with ESR",
-        content: "",
-      },
-      {
-        heading: "Blood Group",
-        content: "",
-      },
-      {
-        heading: "Blood Sugar Random",
-        content: "",
-      },
-      {
-        heading: "TSH",
-        content: "",
-      },
-      {
-        heading: "V.D.R.L.",
-        content: "",
-      },
-      {
-        heading: "HBSAG",
-        content: "",
-      },
-      {
-        heading: "HIV I&II",
-        content: "",
-      },
-      {
-        heading: "HCV",
-        content: "",
-      },
-      {
-        heading: "Urine Routine",
-        content: "",
-      },
+    "id": 5,
+    "name": "HVD ANC 1 PACKAGE",
+    "url": "hvd-anc1-package",
+    "image": "/packages/anc1.jpg",
+    "description": [
+      { "heading": "Complete Blood Count with ESR", "content": "" },
+      { "heading": "Blood Group", "content": "" },
+      { "heading": "Blood Sugar Random", "content": "" },
+      { "heading": "TSH", "content": "" },
+      { "heading": "V.D.R.L.", "content": "" },
+      { "heading": "HBSAG", "content": "" },
+      { "heading": "HIV I&II", "content": "" },
+      { "heading": "HCV", "content": "" },
+      { "heading": "Urine Routine", "content": "" }
     ],
-    price: "2999",
-    cancelled: "3000",
+    "price": "2999",
+    "cancelled": "3000"
   },
   {
-    id: 6,
-    name: "HVD ANC 2 PACKAGE",
-    url: "hvd-anc2-package",
-    image: "/packages/anc2.png",
-    description: [
-      {
-        heading: "Complete Blood Count with ESR",
-        content: "",
-      },
-      {
-        heading: "Blood Group",
-        content: "",
-      },
-      {
-        heading: "Blood Sugar Random",
-        content: "",
-      },
-      {
-        heading: "FREE TFT (FT3, FT4, TSH)",
-        content: "",
-      },
-      {
-        heading: "HPLC",
-        content: "",
-      },
-      {
-        heading: "V.D.R.L.",
-        content: "",
-      },
-      {
-        heading: "HBsAG (Quantitative)",
-        content: "",
-      },
-      {
-        heading: "HIV I&II (Quantitative)",
-        content: "",
-      },
-      {
-        heading: "HCV (Quantitative)",
-        content: "",
-      },
-      {
-        heading: "Urine Routine",
-        content: "",
-      },
+    "id": 6,
+    "name": "HVD ANC 2 PACKAGE",
+    "url": "hvd-anc2-package",
+    "image": "/packages/anc2.png",
+    "description": [
+      { "heading": "Complete Blood Count with ESR", "content": "" },
+      { "heading": "Blood Group", "content": "" },
+      { "heading": "Blood Sugar Random", "content": "" },
+      { "heading": "FREE TFT (FT3, FT4, TSH)", "content": "" },
+      { "heading": "HPLC", "content": "" },
+      { "heading": "V.D.R.L.", "content": "" },
+      { "heading": "HBsAG (Quantitative)", "content": "" },
+      { "heading": "HIV I&II (Quantitative)", "content": "" },
+      { "heading": "HCV (Quantitative)", "content": "" },
+      { "heading": "Urine Routine", "content": "" }
     ],
-    price: "4499",
-    cancelled: "3000",
+    "price": "4499",
+    "cancelled": "3000"
   },
   {
-    id: 7,
-    name: "POLYCYSTIC OVARIAN DISEASE (PCOD) PROFILE",
-    url: "polycystic-ovarian-desease-profile",
-    image: "/packages/pcod.jpg",
-    description: [
-      {
-        heading: "Glucose Fasting",
-        content: "",
-      },
-      {
-        heading: "LH (Leutinizing Hormone)",
-        content: "",
-      },
-      {
-        heading: "Prolactin (PRL)",
-        content: "",
-      },
-      {
-        heading: "FSH (Follicle Stimulating Hormone)",
-        content: "",
-      },
-      {
-        heading: "Thyroid Stimulating Hormone (TSH)",
-        content: "",
-      },
-      {
-        heading: "Insulin Fasting",
-        content: "",
-      },
-      {
-        heading: "Testosterone Total",
-        content: "",
-      },
-      {
-        heading: "Dehydroepiandrosterone Sulphate (DHEA-S)",
-        content: "",
-      },
+    "id": 7,
+    "name": "POLYCYSTIC OVARIAN DISEASE (PCOD) PROFILE",
+    "url": "polycystic-ovarian-desease-profile",
+    "image": "/packages/pcod.jpg",
+    "description": [
+      { "heading": "Glucose Fasting", "content": "" },
+      { "heading": "LH (Leutinizing Hormone)", "content": "" },
+      { "heading": "Prolactin (PRL)", "content": "" },
+      { "heading": "FSH (Follicle Stimulating Hormone)", "content": "" },
+      { "heading": "Thyroid Stimulating Hormone (TSH)", "content": "" },
+      { "heading": "Testosterone Total", "content": "" },
+      { "heading": "Insulin Fasting", "content": "" },
+      { "heading": "Androstenedione", "content": "" },
+      { "heading": "Vitamin B12", "content": "" },
+      { "heading": "25 Hydroxy Vitamin D", "content": "" },
+      { "heading": "DHEA – SO4", "content": "" }
     ],
-    price: "2800",
-    cancelled: "3000",
-  },
-  {
-    id: 8,
-    name: "POLYCYSTIC OVARIAN DISEASE (PCOD) PROFILE ADVANCE",
-    url: "polycystic-ovarian-desease-profile-advance",
-    image: "/packages/pcodAdv.avif",
-    description: [
-      {
-        heading: "Glucose Fasting",
-        content: "",
-      },
-      {
-        heading: "LH (Leutinizing Hormone)",
-        content: "",
-      },
-      {
-        heading: "Prolactin (PRL)",
-        content: "",
-      },
-      {
-        heading: "FSH (Follicle Stimulating Hormone)",
-        content: "",
-      },
-      {
-        heading: "Thyroid Stimulating Hormone (TSH)",
-        content: "",
-      },
-      {
-        heading: "Insulin Fasting",
-        content: "",
-      },
-      {
-        heading: "Testosterone Total",
-        content: "",
-      },
-      {
-        heading: "Dehydroepiandrosterone Sulphate (DHEA-S)",
-        content: "",
-      },
-      {
-        heading: "Anti Mullerian Hormone (AMH)",
-        content: "",
-      },
-      {
-        heading: "Lipid Profile",
-        content: "",
-      },
-      {
-        heading: "Basic Homa -IR (Insulin Resistance Index)",
-        content: "",
-      },
-    ],
-    price: "4600",
-    cancelled: "3000",
-  },
-];
+    "price": "3199",
+    "cancelled": "3500"
+  }
+]
